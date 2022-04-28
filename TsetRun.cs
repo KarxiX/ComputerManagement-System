03d9db8453df6a75257b0d96d11c7952670deb67
KarxiX <95745798+KarxiX@users.noreply.github.com>
2022-04-28 11:33:52 +0800

    Add files via upload

---
+++ TsetRun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ComputerDB001
+{
+    class TsetRun
+    {
+        static void Main(string[] args)
+        {
+            Menus m = new Menus();
+            m.Menuss();
+            Console.ReadKey();
+        }
+    }
+}
